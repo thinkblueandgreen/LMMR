c52cc8e1b44930edc08423982c70c5f5d978c18e
lahawkin <lhawk1000@gmail.com>
2019-10-24 21:03:16 -0700

    yelp api

--- script.js
+++ script.js
@@ -1,25 +1,44 @@
- 
-  var key = "YXCxotPOS3exx01x7bRBAT1rn1MHvH0k";
-    var ticketMasterURL = "https://app.ticketmaster.com/discovery/v2/events.json?apikey=sVCX8fpkv0OR2ToB2r8VZyOHoRua6l6i";
-  console.log('hello'); 
-    $.ajax({
-      url: ticketMasterURL,
-      method: "GET"
-    })
-  
-      .then(function (response) {
-          console.log(response); 
 
-          var events = response._embedded.events; 
-          events.forEach(element => {
+var key = "YXCxotPOS3exx01x7bRBAT1rn1MHvH0k";
+var ticketMasterURL = "https://app.ticketmaster.com/discovery/v2/events.json?keyword=basketball&apikey=sVCX8fpkv0OR2ToB2r8VZyOHoRua6l6i";
+console.log('hello');
+$.ajax({
+    url: ticketMasterURL,
+    method: "GET"
+})
 
-            console.log(element.name); 
-              
-          });
-          
+    .then(function (response) {
+        console.log(response);
 
-      });
-      
-      
+        var events = response._embedded.events;
+        events.forEach(element => {
 
-  
+            console.log(element.name);
+
+        });
+
+
+    });
+
+
+var data = null;
+var xhr = new XMLHttpRequest();
+xhr.open("GET", "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?text=coffee&latitude=37.786882&longitude=-122.399972");
+xhr.setRequestHeader("Authorization", "Bearer 1BeBj-4omHzaOR1JLPwH5DG5o3hVEsXNwnynnclWHxfKwNztWHnnV8ti4WOk3vArHSyRIRKOxLD93LCoVuCG08tb2UJR3Bved1WHYnWxwLVsGDObDgt6it2Zr2uyXXYx");
+xhr.addEventListener("readystatechange", function () {
+    if (this.readyState === 4) {
+        data = JSON.parse(this.responseText);
+        console.log(data);
+    }
+});
+xhr.send(data);
+
+
+
+
+
+
+
+
+
+
