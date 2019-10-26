c7b40773524b63e274804b3e3953f3759ba41e83
lahawkin <lhawk1000@gmail.com>
2019-10-26 12:31:06 -0700

    laura 10/26/2019 removed a console log (hello)

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 
 var key = "YXCxotPOS3exx01x7bRBAT1rn1MHvH0k";
 var ticketMasterURL = "https://app.ticketmaster.com/discovery/v2/events.json?keyword=basketball&apikey=sVCX8fpkv0OR2ToB2r8VZyOHoRua6l6i";
-console.log('hello');
+
+
 $.ajax({
     url: ticketMasterURL,
     method: "GET"
