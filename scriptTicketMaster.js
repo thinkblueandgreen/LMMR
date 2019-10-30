8ea1c161fde244dce4d4149c49a6c5c65573c414
lahawkin <lhawk1000@gmail.com>
2019-10-29 21:52:32 -0700

    got local bars for each event

--- scriptTicketMaster.js
+++ scriptTicketMaster.js
@@ -17,6 +17,7 @@ $("#run-search").on('click', function (event) {
 
             for (i = 0; i < 5; i++) {
                 var gameName = response._embedded.events[i].name;
+                
                 var location = response._embedded.events[i]._embedded.venues[0].name;
                 var address = response._embedded.events[i]._embedded.venues[0].address.line1;
                 var city = response._embedded.events[i]._embedded.venues[0].city.name;
@@ -28,25 +29,37 @@ $("#run-search").on('click', function (event) {
                 var $logEachGameDetail = $("<div>");
                 $logEachGameDetail.append("<h4>" + gameName + "</h4>" + "Stadium : " + location + "<br>" + address + "<br>" + city + ", " + countryName + "<br>" + "Date : " + dateOfEvent + ", " + timeofEvent);
                 $("#searchDisplaySection").append($logEachGameDetail);
-            }
 
-            var zipCode = response._embedded.events[0]._embedded.venues[0].postalCode; 
 
-            // now for the food!
+                var zipCode = response._embedded.events[i]._embedded.venues[0].postalCode;
+
+                // now for the beer!
 
                 var data = null;
                 var xhr = new XMLHttpRequest();
-                
-                xhr.open("GET", "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?location=" + zipCode); 
+
+                xhr.open("GET", "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?location=" + zipCode);
                 xhr.setRequestHeader("Authorization", "Bearer 1BeBj-4omHzaOR1JLPwH5DG5o3hVEsXNwnynnclWHxfKwNztWHnnV8ti4WOk3vArHSyRIRKOxLD93LCoVuCG08tb2UJR3Bved1WHYnWxwLVsGDObDgt6it2Zr2uyXXYx");
                 xhr.addEventListener("readystatechange", function () {
                     if (this.readyState === 4) {
                         data = JSON.parse(this.responseText);
                         console.log(data);
-                    }
-                });
-                xhr.send(data);
 
-            }
-        )
+                        
+
+                        data.businesses.forEach(restaurant => {              
+                            restaurant.categories.forEach(category => {
+                                if (category.alias === 'bars'){
+                                    //logs the local bars to the console
+                                    console.log(restaurant.name); 
+                                };
+                         });       
+                        
+                    })
+                }
+                });
+    xhr.send(data);
+
+}
+        })
 })
